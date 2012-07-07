de2504beac9fd7113032493df8d6071d57ba9462
John <john@arch-virt-home>
2012-07-07 17:51:02 +0300

    imped model test

--- test/unit/vote_test.rb
+++ test/unit/vote_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can insert positive" do
+    rec = nil
+    assert_nothing_raised do
+      rec = Vote.create(:id => 100, :count => 256)
+    end
+    assert(rec.valid?)
+  end
+
+  test "can insert a record with negative count" do
+    assert_raise(ActiveRecord::RecordInvalid) do
+      Vote.create!(:id => 101, :count => -2)
+    end
+  end
 end
