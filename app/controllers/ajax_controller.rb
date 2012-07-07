cd4377cd7afcf2cac3c1842fbf0316988f98ac9a
john <john@eeepc>
2012-07-07 20:51:15 +0300

    imped proper ajax controler

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -2,17 +2,18 @@ class AjaxController < ApplicationController
   layout false
 
   def index
-    @json = {
-      :id => Vote.order(:id).select(:id).to_a
-    }
+    @json = Vote.order(:id).select(:id).to_a
     render 'json'
   end
 
   def vote
-    @json = {
-      :name => 'vote',
-      :other => 321,
-    }
+    v = Vote.find_by_id(params[:id])
+    if v
+      v.count = v.count + 1
+      v.save
+    end
+
+    @json = Vote.order(:id).to_a
     render 'json'
   end
 end
--- spec/controllers/ajax_controller_spec.rb
+++ spec/controllers/ajax_controller_spec.rb
@@ -5,13 +5,34 @@ describe AjaxController do
   describe 'index' do
     fixtures :votes
 
-    it "assigns json IDs" do
+    it "index returns vote ids" do
       get :index
-      json = assigns(:json)
-      json.should have_key(:id)
-      json[:id].should be_a_kind_of(Array)
-      json[:id].should == Vote.select(:id).order(:id).to_a
+      assigns(:json).should == Vote.select(:id).order(:id).to_a
     end
 
+    it "vote increments an existing vote" do
+      prev = Vote.order(:id).to_a
+
+      get :vote, :id => 3
+
+      prev.each do |p|
+        Vote.find_by_id(p[:id]).count.should == p[:count] unless p[:id] == 3
+        Vote.find_by_id(p[:id]).count.should == p[:count] + 1 if p[:id] == 3
+      end
+
+      assigns(:json).should == Vote.order(:id).to_a
+    end
+
+    it "vote doesnot increment an unknown vote" do
+      prev = Vote.order(:id).to_a
+
+      get :vote, :id => 100
+
+      prev.each do |p|
+        Vote.find_by_id(p[:id]).count.should == p[:count]
+      end
+
+      assigns(:json).should == Vote.order(:id).to_a
+    end
   end
 end
