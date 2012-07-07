2cb747cd8408131382dfe5fa73a7723bd264458b
John <john@arch-virt-home>
2012-07-07 12:59:10 +0300

    new fresh voter application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voter::Application.config.secret_token = '8abfd18dacbb232c67cf006669cbb7e619f950fae643e8a63b4e93e6f6d209f1fc7fac5010108cd844f8cff505ca5b425e918be213cca1ade26c096e03e4a3d0'
